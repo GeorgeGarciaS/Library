2698e5a2f877aaf6c94f5caf0a3f4ee1c9455eaf
GeorgeGarciaS <jorgegs2000@gmail.com>
2021-07-17 11:44:26 +1000

    Hardcoded passwords to solve problem detailed in#6

--- LibraryApi/mongoDbConfig.js
+++ LibraryApi/mongoDbConfig.js
@@ -1,40 +1,15 @@
 const mongoose = require('mongoose');
-const fs = require('fs');
 
-// extract key from file
-function fetchKeys() {
-  let keys = false;
-  fs.readFile('./Keys.json', 'utf8', async (error, jsonString) => {
-    if (error) {
-      console.log('Error fetching JSON:', error);
-    }
-    try {
-      keys = JSON.parse(jsonString);
-    } catch (err) {
-      console.log('Error parsing JSON string:', err);
-    }
-    return keys;
-  });
-}
+// Set up mongoose connection
+const dbUser = 'george';
+const dbPassword = '12345';
+const mongoDb = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.vwl9k.mongodb.net/myFirstDatabase?retryWrites=true`;
 
-async function setMongoDb() {
-  const keys = await fetchKeys();
-  if (keys) {
-    const mongoDbLink = keys.serverLink;
-    mongoDbLink.replace('<User>', keys.dbUser);
-    mongoDbLink.replace('<Password>', keys.dbPassword);
-
-    const mongoDb = mongoDbLink;
-
-    mongoose.connect(
-      mongoDb,
-      {
-        useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
-      },
-    );
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'mongo connection error'));
-  }
-}
-
-setMongoDb();
+mongoose.connect(
+  mongoDb,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
+  },
+);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));

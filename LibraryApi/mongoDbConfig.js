3f3c3ed17dc0d9c901c6b56f51bd85ee24766f71
GeorgeGarciaS <jorgegs2000@gmail.com>
2021-07-17 10:48:24 +1000

    Fixed fetch error handling

--- LibraryApi/mongoDbConfig.js
+++ LibraryApi/mongoDbConfig.js
@@ -2,30 +2,39 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 
 // extract key from file
-let keys = false;
-fs.readFile('tests/bookInfo.json', 'utf8', (err, jsonString) => {
-  if (err) {
-    throw err;
+function fetchKeys() {
+  let keys = false;
+  fs.readFile('./Keys.json', 'utf8', async (error, jsonString) => {
+    if (error) {
+      console.log('Error fetching JSON:', error);
+    }
+    try {
+      keys = JSON.parse(jsonString);
+    } catch (err) {
+      console.log('Error parsing JSON string:', err);
+    }
+    return keys;
+  });
+}
+
+async function setMongoDb() {
+  const keys = await fetchKeys();
+  if (keys) {
+    const mongoDbLink = keys.serverLink;
+    mongoDbLink.replace('<User>', keys.dbUser);
+    mongoDbLink.replace('<Password>', keys.dbPassword);
+
+    const mongoDb = mongoDbLink;
+
+    mongoose.connect(
+      mongoDb,
+      {
+        useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
+      },
+    );
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'mongo connection error'));
   }
-  try {
-    keys = JSON.parse(jsonString);
-  } catch (error) {
-    console.log('Error parsing JSON string:', error);
-  }
-});
+}
 
-// set up mongoose connection
-const mongoDbLink = keys.serverLink;
-mongoDbLink.replace('<User>', keys.dbUser);
-mongoDbLink.replace('<Password>', keys.dbPassword);
-
-const mongoDb = mongoDbLink;
-
-mongoose.connect(
-  mongoDb,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
-  },
-);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'mongo connection error'));
+setMongoDb();

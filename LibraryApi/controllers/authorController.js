2143512aa9a55b5aaa9ec7f8a39a8f896733e133
GeorgeGarciaS <jorgegs2000@gmail.com>
2021-07-17 12:58:14 +1000

    Created base logic to get author's details

--- LibraryApi/controllers/authorController.js
+++ LibraryApi/controllers/authorController.js
@@ -17,8 +17,36 @@ exports.authorList = function (req, res, next) {
 };
 
 // details for a specific author
-exports.authorDetail = function (req, res) {
-  return res.status(200).json({});
+exports.authorDetail = function (req, res, next) {
+  async.parallel({
+    author(callback) {
+      Author.findById(req.params.id)
+        .exec(callback);
+    },
+    authors_books(callback) {
+      Book.find({ author: req.params.id })
+        .exec(callback);
+    },
+  },
+  (error, results) => {
+    if (error) {return next(error);} // error in API usage
+    if (results.author === null) {
+      // no results
+      const err = Error('Author not found');
+      err.name = 'database error';
+      err.status = 404;
+      return next(err);
+    }
+    // successful
+    return res.status(200).json({
+      // pass only necessary data
+      first_name: results.author.first_name,
+      family_name: results.author.family_name,
+      date_of_birth: results.author.date_of_birth_formatted,
+      date_of_death: results.author.date_of_death_formatted,
+      books: results.authors_books,
+    });
+  });
 };
 
 // handle author create
